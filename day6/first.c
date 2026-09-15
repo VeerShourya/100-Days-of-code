8b1acaa3defd5ca93691d4bc62d2ae2bb6102911
VeerShourya <veershourya999@gmail.com>
2026-09-16 00:21:30 +0530

    Add files via upload

---
+++ day6/first.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    int num;
+
+    printf("Enter an integer: ");
+    scanf("%d", &num);
+
+    if(num % 2 == 0)
+    {
+        printf("The number is even.");
+    }
+    else
+    {
+        printf("The number is odd.");
+    }
+
+    return 0;
+}
